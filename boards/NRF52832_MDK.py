e5fab10e21cb8db9cbc3c36f3bd2c860791e7c61
makerdiary <zelin@makerdiary.co>
2017-03-29 09:52:23 +0800

    add a new board: nRF52832-MDK. For more hardware information, please visit: https://wiki.makerdiary.co/nrf52832-mdk

---
+++ boards/NRF52832_MDK.py
@@ -0,0 +1,117 @@
+#!/bin/false
+# This file is part of Espruino, a JavaScript interpreter for Microcontrollers
+#
+# Copyright (C) 2013 Gordon Williams <gw@pur3.co.uk>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# ----------------------------------------------------------------------------------------
+# This file contains information for a specific board - the available pins, and where LEDs,
+# Buttons, and other in-built peripherals are. It is used to build documentation as well
+# as various source and header files for Espruino.
+# ----------------------------------------------------------------------------------------
+
+import pinutils;
+
+info = {
+ 'name' : "nRF52832 Micro Development Kit",
+ 'link' :  [ "https://wiki.makerdiary.co/nrf52832-mdk" ],
+ 'default_console' : "EV_SERIAL1",
+ 'default_console_tx' : "D20",
+ 'default_console_rx' : "D19",
+ 'default_console_baudrate' : "9600",
+ # Number of variables can be WAY higher on this board
+ 'variables' : 2000, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
+ #'bootloader' : 1,
+ 'binary_name' : 'espruino_%v_nrf52832_mdk.bin',
+ 'build' : {
+   'optimizeflags' : '-Os',
+   'libraries' : [
+     'BLUETOOTH',
+     'NET',
+     'GRAPHICS',
+     'CRYPTO',
+     'NFC',
+     'NEOPIXEL'
+     #'HASHLIB'
+     #'FILESYSTEM'
+     #'TLS'
+   ],
+   'makefile' : [
+     'DEFINES += -DBOARD_PCA10040 -DPCA10040'
+   ]
+ }
+};
+
+chip = {
+  'part' : "NRF52832",
+  'family' : "NRF52",
+  'package' : "QFN48",
+  'ram' : 64, # Currently there is a bug with NRF52 preview DK's RAM but this will be fixed next revision.
+  'flash' : 512,
+  'speed' : 64,
+  'usart' : 1, 
+  'spi' : 3,
+  'i2c' : 2,
+  'adc' : 1,
+  'dac' : 0,
+  'saved_code' : {
+    'address' : ((120 - 3) * 4096), # Bootloader takes pages 120-127
+    'page_size' : 4096,
+    'pages' : 3,
+    'flash_available' : 512 - ((31 + 8 + 3)*4) # Softdevice uses 31 pages of flash, bootloader 8, code 3. Each page is 4 kb. 
+  },
+};
+
+devices = {
+  'LED1' : { 'pin' : 'D22', 'inverted' : False},
+  'LED2' : { 'pin' : 'D23', 'inverted' : False},
+  'LED3' : { 'pin' : 'D24', 'inverted' : False},
+  'RX_PIN_NUMBER' : { 'pin' : 'D19'},
+  'TX_PIN_NUMBER' : { 'pin' : 'D20'},
+};
+
+# left-right, or top-bottom order
+board = {
+  'left' : [ 'VIN', '5V', 'GND', 'PD4','PD5','PD6','PD7','PD8','PD9','PD10','PD11','PD12','PD13','PD14','PD15','PD16','PD17','PD18'],
+  'right' : [ '3V3', 'GND', 'RST', 'CLK', 'DIO','TDO','TDI', 'TXD','RXD','PD3','PD2','PD31','PD30','PD29','PD28','PD27','PD26','PD25'],
+};
+board["_css"] = """
+""";
+
+def get_pins():
+  pins = pinutils.generate_pins(0,31) # 32 General Purpose I/O Pins.
+  pinutils.findpin(pins, "PD0", True)["functions"]["XL1"]=0;
+  pinutils.findpin(pins, "PD1", True)["functions"]["XL2"]=0;
+  pinutils.findpin(pins, "PD5", True)["functions"]["3.3"]=0;
+  pinutils.findpin(pins, "PD6", True)["functions"]["3.3"]=0;
+  pinutils.findpin(pins, "PD7", True)["functions"]["3.3"]=0;
+  pinutils.findpin(pins, "PD8", True)["functions"]["3.3"]=0;
+  pinutils.findpin(pins, "PD9", True)["functions"]["NFC1"]=0;
+  pinutils.findpin(pins, "PD10", True)["functions"]["NFC2"]=0;
+  pinutils.findpin(pins, "PD13", True)["functions"]["3.3"]=0;
+  pinutils.findpin(pins, "PD14", True)["functions"]["3.3"]=0;
+  pinutils.findpin(pins, "PD15", True)["functions"]["3.3"]=0;
+  pinutils.findpin(pins, "PD16", True)["functions"]["3.3"]=0;
+  pinutils.findpin(pins, "PD17", True)["functions"]["3.3"]=0;
+  pinutils.findpin(pins, "PD18", True)["functions"]["3.3"]=0;
+  pinutils.findpin(pins, "PD19", True)["functions"]["RXD"]=0;
+  pinutils.findpin(pins, "PD20", True)["functions"]["TXD"]=0;
+  pinutils.findpin(pins, "PD22", True)["functions"]["LED_1"]=0;
+  pinutils.findpin(pins, "PD23", True)["functions"]["LED_2"]=0;
+  pinutils.findpin(pins, "PD24", True)["functions"]["LED_3"]=0;
+  pinutils.findpin(pins, "PD2", True)["functions"]["ADC1_IN0"]=0;
+  pinutils.findpin(pins, "PD3", True)["functions"]["ADC1_IN1"]=0;
+  pinutils.findpin(pins, "PD4", True)["functions"]["ADC1_IN2"]=0;
+  pinutils.findpin(pins, "PD5", True)["functions"]["ADC1_IN3"]=0;
+  pinutils.findpin(pins, "PD28", True)["functions"]["ADC1_IN4"]=0;
+  pinutils.findpin(pins, "PD29", True)["functions"]["ADC1_IN5"]=0;
+  pinutils.findpin(pins, "PD30", True)["functions"]["ADC1_IN6"]=0;
+  pinutils.findpin(pins, "PD31", True)["functions"]["ADC1_IN7"]=0;
+  # everything is non-5v tolerant
+  for pin in pins:
+    pin["functions"]["3.3"]=0;
+  #The boot/reset button will function as a reset button in normal operation. Pin reset on PD21 needs to be enabled on the nRF52832 device for this to work.
+  return pins
